74adf40d6ad9ed239d4c29c3ac21281285b7fb0d
Matt Hoyle <code@deployable.co>
2017-04-01 15:39:56 +0000

    merge vbox and ec2 into ansible
    single playbook successfully building both.
    ec2 booting, vbox complaining about an extlinux file missing

--- test/test.tf
+++ test/test.tf
@@ -12,19 +12,18 @@ resource "aws_instance" "built" {
   subnet_id     = "subnet-85f0fcc3"
   key_name      = "mattapsydney"
   vpc_security_group_ids  = ["sg-2b52e14f"]
-  private_ip = "172.30.1.201"
 }
 
 # Debug dhcp interface
-resource "aws_network_interface" "docker" {
-  subnet_id       = "subnet-85f0fcc3"
-#  private_ips     = [""]
-  security_groups = ["sg-2b52e14f"]
-  attachment {
-    instance     = "${aws_instance.built.id}"
-    device_index = 1
-  }
-}
+# resource "aws_network_interface" "docker" {
+#   subnet_id       = "subnet-85f0fcc3"
+# #  private_ips     = [""]
+#   security_groups = ["sg-2b52e14f"]
+#   attachment {
+#     instance     = "${aws_instance.built.id}"
+#     device_index = 1
+#   }
+# }
 
 output "address" {
   value = "${aws_instance.built.public_ip}"
