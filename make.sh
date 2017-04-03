75d0242150264049a4d0c111bd29b6ccca16a2a1
Matt Hoyle <code@deployable.co>
2017-04-03 06:10:26 +0000

    docco

--- make.sh
+++ make.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
 cmd=${1:-}
+shift
+
+# Default the command
 if [ -z "$cmd" ]; then
   cmd=build
 fi
 
+# DEBUG environment var
 if [ -n "${DEBUG:-}" ]; then
   PACKER_ARGS="-debug"
 fi
 
+# ## Builds
 build_virtualbox(){
   pkill -f 'packer build alpine-vagrant.json'
   rm -rf output-virtualbox-iso/
@@ -19,8 +24,19 @@ build_ami(){
   packer build $PACKER_ARGS debian-alpine.json
 }
 
+
+# ## Shortcuts
+
 build(){
   build_ami
+}
+
+ami(){
+  build_ami
+}
+
+virtualbox(){
+  build_virtualbox
 }
 
 $cmd "$@"
